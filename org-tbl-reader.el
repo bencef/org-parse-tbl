d8d800ca0c55f6660234f4270611bac600e6ba9c
Bence Fabian <begnoc@gmail.com>
2017-09-13 23:26:28 +0200

    initial commit

---
+++ org-tbl-reader.el
@@ -0,0 +1,29 @@
+(defun get-headers (tbl)
+  "Get the name of headers from a table."
+  (car tbl))
+
+(defun get-data (tbl)
+  "Get unprocessed rows of data"
+  (let (acc)
+    (dolist (row (cdr tbl) (nreverse acc))
+      (when (listp row)
+        (push row acc)))))
+
+(defun parse-row (headers readers row)
+  "Collect the elements of row into a hash-map
+using headers as keys and applying readers."
+  (let ((h (make-hash-table :test #'equal)))
+    (loop for key in headers
+          for reader in readers
+          for elt in row
+          do (setf (gethash key h) (funcall reader elt)))
+    h))
+
+(defun parse-tbl (tbl readers)
+  "Parse data from a table into a list of hash-maps."
+  (let ((headers (get-headers tbl))
+        (data (get-data tbl)))
+    (when (not (= (length headers) (length readers)))
+      (error "Length of headers doesn't match the lenght of readers."))
+    (loop for row in data
+          collect (parse-row headers readers row))))
